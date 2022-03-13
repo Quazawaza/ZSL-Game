e20fd97cdcb9599a9df90c56e4630912b26921af
Quazawaza <tomaszciurkowski33@gmail.com>
2022-03-13 18:26:35 +0100

    pierwszy comit

---
+++ Assets/Card.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCard", menuName = "Card")]
+public class Card : ScriptableObject {
+
+    public new string name;
+    public string description;
+
+    public Sprite artwork;
+    public Sprite row;
+
+    public int attack;
+
+    public void Print ()
+    {
+        Debug.Log(name + ": " + description + " Karta ma " + attack + " ataku");
+    }
+
+}
